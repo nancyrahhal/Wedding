81a64f1828fdd85208478cbdaf6dbf4daee2dab1
Nancy Rahhal <nancyrahhal97@gmail.com>
2026-07-26 05:07:35 +0300

    4 attendees

--- Wedding/src/App.jsx
+++ Wedding/src/App.jsx
@@ -716,9 +716,9 @@ function App() {
                   className="rsvp-field rsvp-attendees-field"
                   type="text"
                   name="attendees"
-                  value="1"
+                  value="4"
                   readOnly
-                  aria-label={invitationLanguage === 'arabic' ? 'عدد الحضور: 1' : 'Number of attendees: 1'}
+                  aria-label={invitationLanguage === 'arabic' ? 'عدد الحضور: 4' : 'Number of attendees: 4'}
                 />
 
                 <textarea
