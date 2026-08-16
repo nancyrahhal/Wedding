2450a61bb0debc83c0f1e801626375f2fa046788
Nancy Rahhal <nancyrahhal97@gmail.com>
2026-08-16 17:43:32 +0300

    remove whish

--- Wedding/src/App.jsx
+++ Wedding/src/App.jsx
@@ -646,7 +646,7 @@ function App() {
             </button>
           </div>
 
-          <div
+          {/* <div
             ref={venuePhotoRef}
             className="canvas-viewport gift-page-viewport"
             style={{
@@ -729,7 +729,7 @@ function App() {
               <img src={asset('music-icon-optimized.png')} alt="" aria-hidden="true" />
               {isMuted && <span className="music-toggle__slash" aria-hidden="true" />}
             </button>
-          </div>
+          </div> */}
 
           <div
             className="canvas-viewport gift-options-viewport"
